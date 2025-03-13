4305859ed1fdf45157c87667d683cb8b1986e903
RichiEee3704 <ya.richard23@gmail.com>
2025-03-13 14:50:51 -0400

    vision sim stuf

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,15 +4,20 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.RobotContainer;
+// import frc.robot.commands.RobotContainer;
 import frc.robot.subsystem.aIntake;
 import frc.robot.subsystem.cIntake;
 import frc.robot.subsystem.drivetrain;
 import frc.robot.subsystem.operatorinterface;
 import frc.robot.subsystem.lift;
+import frc.robot.simulation.VisionSim;
+
 
 public class Robot extends TimedRobot {
 
@@ -21,30 +26,51 @@ public class Robot extends TimedRobot {
   public aIntake algae;
   public cIntake coral;
   public lift elevator;
-  public RobotContainer robotContainer;
   public Command getAutonomousCommand;
+  private VisionSim visionSim;
+  private double leftJoy;
+  private double rightJoy;
+  private boolean leftBumper;
+  private boolean rightBumper;
 
   @Override
   public void robotInit() {
     drive = drivetrain.getInstance();
-    oi= operatorinterface.getInstance();
+    oi = operatorinterface.getInstance();
     algae = aIntake.getInstance();
     coral = cIntake.getInstance();
     elevator = lift.getInstance();
-    robotContainer = new RobotContainer();
+    visionSim = new VisionSim("main", "cameraName");
+
+    // No need for SendableRegistry here for these simple values
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-    
+
+    // Get movement values from SmartDashboard
+    leftJoy = SmartDashboard.getNumber("Left Joystick", 0.0); // Left joystick input
+    rightJoy = SmartDashboard.getNumber("Right Joystick", 0.0); // Right joystick input
+    leftBumper = SmartDashboard.getBoolean("Left Bumper", false); // Left bumper button state
+    rightBumper = SmartDashboard.getBoolean("Right Bumper", false); // Right bumper button state
+
+    // Update the robot's movement based on these values
+    drive.Drivecode(leftJoy, rightJoy, leftBumper, rightBumper);
+
+    // Update VisionSim with the new robot pose
+    Pose2d robotPose = drive.getPose(); // Get the updated pose from the drivetrain
+    visionSim.update(robotPose);
+
+    // Display debug field for visualization in SmartDashboard
+    SmartDashboard.putData("Field", visionSim.getDebugField());
   }
 
   @Override
   public void autonomousInit() {
-    if(getAutonomousCommand != null){
+    if (getAutonomousCommand != null) {
       getAutonomousCommand.schedule();
-  }
+    }
   }
 
   @Override
@@ -53,9 +79,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    if(getAutonomousCommand != null){
+    if (getAutonomousCommand != null) {
       getAutonomousCommand.cancel();
-  }
+    }
   }
 
   @Override
