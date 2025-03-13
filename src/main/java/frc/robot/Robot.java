3d1e90d13dbaf9a8184cd9fab7117bf2fd8299a4
RichiEee3704 <ya.richard23@gmail.com>
2025-03-13 14:52:10 -0400

    idk

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -28,10 +27,7 @@ public class Robot extends TimedRobot {
   public lift elevator;
   public Command getAutonomousCommand;
   private VisionSim visionSim;
-  private double leftJoy;
-  private double rightJoy;
-  private boolean leftBumper;
-  private boolean rightBumper;
+
 
   @Override
   public void robotInit() {
@@ -42,28 +38,18 @@ public class Robot extends TimedRobot {
     elevator = lift.getInstance();
     visionSim = new VisionSim("main", "cameraName");
 
-    // No need for SendableRegistry here for these simple values
+  
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
 
-    // Get movement values from SmartDashboard
-    leftJoy = SmartDashboard.getNumber("Left Joystick", 0.0); // Left joystick input
-    rightJoy = SmartDashboard.getNumber("Right Joystick", 0.0); // Right joystick input
-    leftBumper = SmartDashboard.getBoolean("Left Bumper", false); // Left bumper button state
-    rightBumper = SmartDashboard.getBoolean("Right Bumper", false); // Right bumper button state
-
-    // Update the robot's movement based on these values
-    drive.Drivecode(leftJoy, rightJoy, leftBumper, rightBumper);
-
     // Update VisionSim with the new robot pose
     Pose2d robotPose = drive.getPose(); // Get the updated pose from the drivetrain
     visionSim.update(robotPose);
 
-    // Display debug field for visualization in SmartDashboard
-    SmartDashboard.putData("Field", visionSim.getDebugField());
+    
   }
 
   @Override
